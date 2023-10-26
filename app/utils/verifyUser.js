68db77b87f06e05b2f582d7de1943988e5430f17
Ajhar <ajharudin@appventurez.com>
2023-10-26 15:07:08 +0530

    modified authorization in verifyUser

--- app/utils/verifyUser.js
+++ app/utils/verifyUser.js
@@ -3,7 +3,7 @@ const { errorHandler } = require('./error');
 const User = require('../models/user');
 
 exports.verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.cookies.access_token;
   if (!token) return next(errorHandler(401, 'Unauthorized'));
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, 'Forbidden'));
